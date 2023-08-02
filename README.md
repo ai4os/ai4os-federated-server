1fd955605f5f195caaf96997a17d10203884fee8
Ignacio Heredia <iheredia@ifca.unican.es>
2023-08-02 13:02:04 +0200

    feat: add client samples

--- README.md
+++ README.md
@@ -19,10 +19,15 @@ Possible aggregation strategies introduced as *FEDERATED_STRATEGY* (see the impl
 * **Federated Optim strategy**: *"fed_opt"* [3]
 * **Adaptive Federated Optimization using Yogi:** *"fed_yogi"* [3]
 
-
 The associated Docker container for this module can be found in https://github.com/deephdc/DEEP-OC-federated-server.
 
+We provide some [client examples](./fedserver/client_samples/) as a guide for users.
+Users will need to adapt the `endpoint` in those samples to point to their deployed
+Federated server.
+
+
 ### References
+
 [1] McMahan, B., Moore, E., Ramage, D., Hampson, S., & y Arcas, B. A. (2017, April). Communication-efficient learning of deep networks from decentralized data. In Artificial intelligence and statistics (pp. 1273-1282). PMLR.
 
 [2] Li, T., Sahu, A. K., Zaheer, M., Sanjabi, M., Talwalkar, A., & Smith, V. (2020). Federated optimization in heterogeneous networks. Proceedings of Machine learning and systems, 2, 429-450.
---
+++ fedserver/client_samples/keras_client.py
@@ -0,0 +1,74 @@
+"""
+Simple image classification example with Keras and MNIST.
+"""
+
+import certifi
+from pathlib import Path
+
+import flwr as fl
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.datasets import mnist
+
+
+# Load and process data:
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = np.reshape(X_train, (60000, 28, 28, 1))
+X_train = X_train.astype('float32') / 255
+X_test = np.reshape(X_test, (10000, 28, 28, 1))
+X_test = X_test.astype('float32') / 255
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Create a new train/test set for client 1:
+x_train = X_train[:len(X_train)//3]
+y_train = y_train[:len(X_train)//3]
+
+x_test = X_test[:len(X_test)//3]
+y_test = y_test[:len(X_test)//3]
+
+# Model to be trained:
+model = Sequential()
+model.add(Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)))
+model.add((MaxPooling2D((2,2))))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add((MaxPooling2D((2,2))))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(Flatten())
+model.add(Dense(64, activation = 'relu'))
+model.add(Dense(10, activation = 'softmax'))
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = 'rmsprop',
+              metrics = ['accuracy'])
+model.summary()
+
+# Flower client
+class Client1(fl.client.NumPyClient):
+    def get_parameters(self, config):
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(x_train, y_train, epochs=5, batch_size=32)
+        return model.get_weights(), len(x_train), {}
+
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test)
+        return loss, len(x_test), {"accuracy": accuracy}
+
+
+# Connect with the Flower server
+
+############################  FILL ME  #################################################
+end_point = "fedserver-xxxxxxxxxxxxxx.deployments.cloud.ai4eosc.eu"
+########################################################################################
+
+fl.client.start_numpy_client(
+    server_address=f"{end_point}:443",
+    client=Client1(),
+    root_certificates=Path(certifi.where()).read_bytes(),
+    )
