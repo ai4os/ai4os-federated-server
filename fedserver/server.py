38302d97c3ad51c5986a85a3a834f5977b333194
Judith Sáinz-Pardo Díaz <sainzpardo@ifca.unican.es>
2026-03-05 10:24:22 +0100

    fix: fix iso code for codecarbon

--- fedserver/server.py
+++ fedserver/server.py
@@ -13,25 +13,29 @@ import logging
 INFO = logging.INFO
 
 
-FEDERATED_ROUNDS: int = int(os.environ['FEDERATED_ROUNDS'])
-FEDERATED_METRIC = os.environ['FEDERATED_METRIC']
+FEDERATED_ROUNDS: int = int(os.environ["FEDERATED_ROUNDS"])
+FEDERATED_METRIC = os.environ["FEDERATED_METRIC"]
 FEDERATED_MIN_FIT_CLIENTS: int = int(os.environ["FEDERATED_MIN_FIT_CLIENTS"])
-FEDERATED_MIN_AVAILABLE_CLIENTS: int = int(os.environ["FEDERATED_MIN_AVAILABLE_CLIENTS"])
-FEDERATED_STRATEGY: str = os.environ['FEDERATED_STRATEGY']
+FEDERATED_MIN_AVAILABLE_CLIENTS: int = int(
+    os.environ["FEDERATED_MIN_AVAILABLE_CLIENTS"]
+)
+FEDERATED_STRATEGY: str = os.environ["FEDERATED_STRATEGY"]
 MU_FEDPROX = os.environ["MU_FEDPROX"]
 FEDAVGM_SERVER_FL = os.environ["FEDAVGM_SERVER_FL"]
 FEDAVGM_SERVER_MOMENTUM = os.environ["FEDAVGM_SERVER_MOMENTUM"]
-DP_BOOL: bool = os.environ['DP']
+DP_BOOL: bool = os.environ["DP"]
 NOISE_MULTIPLIER = os.environ["NOISE_MULT"]
 CLIPPING_NORM = os.environ["CLIP_NORM"]
-SAMPLED_CLIENTS = os.environ['SAMPLED_CLIENTS']
-METRIC_PRIVACY = os.environ['METRIC_PRIVACY']
-CODE_CARBON: bool = os.environ['CODE_CARBON']
-DATA_CENTER = os.environ['NOMAD_DC']
-if DATA_CENTER == 'iisas-ai4eosc':
-    COUNTRY = 'SVK'
+SAMPLED_CLIENTS = os.environ["SAMPLED_CLIENTS"]
+METRIC_PRIVACY = os.environ["METRIC_PRIVACY"]
+CODE_CARBON: bool = os.environ["CODE_CARBON"]
+DATA_CENTER = os.environ["NOMAD_DC"]
+if DATA_CENTER == "iisas-ai4eosc":
+    COUNTRY = "SVK"
+elif DATA_CENTER == "psnc-ai4eosc":
+    COUNTRY = "POL"
 else:
-    COUNTRY = 'ESP'
+    COUNTRY = "ESP"
 
 
 # Weighted average of the metric:
@@ -54,6 +58,7 @@ def wavg_metric(metrics):
             dict_metrics[fed_metric] = wavg_metric
         return dict_metrics
 
+
 if FEDERATED_STRATEGY == "Federated Averaging (FedAvg)" or FEDERATED_STRATEGY is None:
     strategy = fl.server.strategy.FedAvg(
         min_available_clients=FEDERATED_MIN_FIT_CLIENTS,
@@ -70,58 +75,58 @@ elif FEDERATED_STRATEGY == "Federated Averaging with Momentum (FedAvgM)":
     FEDAVGM_SERVER_FL = float(FEDAVGM_SERVER_FL)
     FEDAVGM_SERVER_MOMENTUM = float(FEDAVGM_SERVER_MOMENTUM)
     if FEDAVGM_SERVER_MOMENTUM > 0:
-        model = tf.keras.models.load_model('initial_model.keras')
+        model = tf.keras.models.load_model("initial_model.keras")
         initial_parameters = ndarrays_to_parameters(model.get_weights())
         strategy = fl.server.strategy.FedAvgM(
-        min_available_clients=FEDERATED_MIN_FIT_CLIENTS,
-        min_fit_clients=FEDERATED_MIN_AVAILABLE_CLIENTS,
-        evaluate_metrics_aggregation_fn=wavg_metric,
-        server_learning_rate=FEDAVGM_SERVER_FL,
-        server_momentum=FEDAVGM_SERVER_MOMENTUM,
-        initial_parameters = initial_parameters
-    )
+            min_available_clients=FEDERATED_MIN_FIT_CLIENTS,
+            min_fit_clients=FEDERATED_MIN_AVAILABLE_CLIENTS,
+            evaluate_metrics_aggregation_fn=wavg_metric,
+            server_learning_rate=FEDAVGM_SERVER_FL,
+            server_momentum=FEDAVGM_SERVER_MOMENTUM,
+            initial_parameters=initial_parameters,
+        )
     else:
         strategy = fl.server.strategy.FedAvgM(
-        min_available_clients=FEDERATED_MIN_FIT_CLIENTS,
-        min_fit_clients=FEDERATED_MIN_AVAILABLE_CLIENTS,
-        evaluate_metrics_aggregation_fn=wavg_metric,
-        server_learning_rate=FEDAVGM_SERVER_FL,
-        server_momentum=FEDAVGM_SERVER_MOMENTUM
-    )
+            min_available_clients=FEDERATED_MIN_FIT_CLIENTS,
+            min_fit_clients=FEDERATED_MIN_AVAILABLE_CLIENTS,
+            evaluate_metrics_aggregation_fn=wavg_metric,
+            server_learning_rate=FEDAVGM_SERVER_FL,
+            server_momentum=FEDAVGM_SERVER_MOMENTUM,
+        )
 elif FEDERATED_STRATEGY == "FedProx strategy (FedProx)":
     MU_FEDPROX = float(MU_FEDPROX)
     strategy = fl.server.strategy.FedProx(
         min_available_clients=FEDERATED_MIN_FIT_CLIENTS,
         min_fit_clients=FEDERATED_MIN_AVAILABLE_CLIENTS,
         evaluate_metrics_aggregation_fn=wavg_metric,
-        proximal_mu = MU_FEDPROX
+        proximal_mu=MU_FEDPROX,
     )
 elif FEDERATED_STRATEGY == "Adaptive Federated Optimization (FedOpt)":
-    model = tf.keras.models.load_model('initial_model.keras')
+    model = tf.keras.models.load_model("initial_model.keras")
     initial_parameters = ndarrays_to_parameters(model.get_weights())
     strategy = fl.server.strategy.FedOpt(
         min_available_clients=FEDERATED_MIN_FIT_CLIENTS,
         min_fit_clients=FEDERATED_MIN_AVAILABLE_CLIENTS,
         evaluate_metrics_aggregation_fn=wavg_metric,
-        initial_parameters = initial_parameters
+        initial_parameters=initial_parameters,
     )
 elif FEDERATED_STRATEGY == "Federated Optimization with Adam (FedAdam)":
-    model = tf.keras.models.load_model('initial_model.keras')
+    model = tf.keras.models.load_model("initial_model.keras")
     initial_parameters = ndarrays_to_parameters(model.get_weights())
     strategy = fl.server.strategy.FedAdam(
         min_available_clients=FEDERATED_MIN_FIT_CLIENTS,
         min_fit_clients=FEDERATED_MIN_AVAILABLE_CLIENTS,
         evaluate_metrics_aggregation_fn=wavg_metric,
-        initial_parameters = initial_parameters
+        initial_parameters=initial_parameters,
     )
 elif FEDERATED_STRATEGY == "Adaptive Federated Optimization using Yogi (FedYogi)":
-    model = tf.keras.models.load_model('initial_model.keras')
+    model = tf.keras.models.load_model("initial_model.keras")
     initial_parameters = ndarrays_to_parameters(model.get_weights())
     strategy = fl.server.strategy.FedYogi(
         min_available_clients=FEDERATED_MIN_FIT_CLIENTS,
         min_fit_clients=FEDERATED_MIN_AVAILABLE_CLIENTS,
         evaluate_metrics_aggregation_fn=wavg_metric,
-        initial_parameters = initial_parameters
+        initial_parameters=initial_parameters,
     )
 elif FEDERATED_STRATEGY == "FedAvgOpt":
     strategy = FedAvgOpt(
@@ -130,27 +135,34 @@ elif FEDERATED_STRATEGY == "FedAvgOpt":
         evaluate_metrics_aggregation_fn=wavg_metric,
     )
 
+
 class AggregateEmissions(type(strategy)):
     def __init__(self, strategy):
         params = strategy.__dict__
         super().__init__(**params)
-        
+
     def aggregate_fit(self, server_round, results, failures):
         server_tracker = OfflineEmissionsTracker(
             country_iso_code=COUNTRY, save_to_file=False
         )
         server_tracker.start()
-        aggregated_parameters, aggregated_metrics = super().aggregate_fit(server_round, results, failures)
+        aggregated_parameters, aggregated_metrics = super().aggregate_fit(
+            server_round, results, failures
+        )
 
         # Emissions from clients
-        client_emissions = [res.metrics["emissions"] for _, res in results if "emissions" in res.metrics]
+        client_emissions = [
+            res.metrics["emissions"] for _, res in results if "emissions" in res.metrics
+        ]
         total_client_emissions = sum(client_emissions) if client_emissions else 0.0
-        
+
         # Server-side emissions
         server_emissions = server_tracker.stop()
-        
+
         total_emissions = total_client_emissions + server_emissions
-        print(f"ROUND {server_round}. Total Carbon Emissions (server and clients): {total_emissions} kg CO2")
+        print(
+            f"ROUND {server_round}. Total Carbon Emissions (server and clients): {total_emissions} kg CO2"
+        )
 
         return aggregated_parameters, {"total_emissions": total_emissions}
 
@@ -158,28 +170,34 @@ class AggregateEmissions(type(strategy)):
 class AggregateEmissionsDP(DifferentialPrivacyServerSideFixedClipping):
     def __init__(self, strategy):
         super().__init__(
-            strategy, 
-            noise_multiplier=float(NOISE_MULTIPLIER), 
-            clipping_norm=float(CLIPPING_NORM), 
-            num_sampled_clients=SAMPLED_CLIENTS
+            strategy,
+            noise_multiplier=float(NOISE_MULTIPLIER),
+            clipping_norm=float(CLIPPING_NORM),
+            num_sampled_clients=SAMPLED_CLIENTS,
         )
-        
+
     def aggregate_fit(self, server_round, results, failures):
         server_tracker = OfflineEmissionsTracker(
             country_iso_code=COUNTRY, save_to_file=False
         )
         server_tracker.start()
-        aggregated_parameters, aggregated_metrics = super().aggregate_fit(server_round, results, failures)
+        aggregated_parameters, aggregated_metrics = super().aggregate_fit(
+            server_round, results, failures
+        )
 
         # Emissions from clients
-        client_emissions = [res.metrics["emissions"] for _, res in results if "emissions" in res.metrics]
+        client_emissions = [
+            res.metrics["emissions"] for _, res in results if "emissions" in res.metrics
+        ]
         total_client_emissions = sum(client_emissions) if client_emissions else 0.0
-        
+
         # Server-side emissions
         server_emissions = server_tracker.stop()
-        
+
         total_emissions = total_client_emissions + server_emissions
-        print(f"ROUND {server_round}. Total Carbon Emissions (server and clients): {total_emissions} kg CO2")
+        print(
+            f"ROUND {server_round}. Total Carbon Emissions (server and clients): {total_emissions} kg CO2"
+        )
 
         return aggregated_parameters, {"total_emissions": total_emissions}
 
@@ -187,32 +205,38 @@ class AggregateEmissionsDP(DifferentialPrivacyServerSideFixedClipping):
 class AggregateEmissionsMDP(MetricDifferentialPrivacyServerSideFixedClipping):
     def __init__(self, strategy):
         super().__init__(
-            strategy, 
-            noise_multiplier=float(NOISE_MULTIPLIER), 
-            clipping_norm=float(CLIPPING_NORM), 
-            num_sampled_clients=SAMPLED_CLIENTS
+            strategy,
+            noise_multiplier=float(NOISE_MULTIPLIER),
+            clipping_norm=float(CLIPPING_NORM),
+            num_sampled_clients=SAMPLED_CLIENTS,
         )
-        
+
     def aggregate_fit(self, server_round, results, failures):
         server_tracker = OfflineEmissionsTracker(
             country_iso_code=COUNTRY, save_to_file=False
         )
         server_tracker.start()
-        aggregated_parameters, aggregated_metrics = super().aggregate_fit(server_round, results, failures)
+        aggregated_parameters, aggregated_metrics = super().aggregate_fit(
+            server_round, results, failures
+        )
 
         # Emissions from clients
-        client_emissions = [res.metrics["emissions"] for _, res in results if "emissions" in res.metrics]
+        client_emissions = [
+            res.metrics["emissions"] for _, res in results if "emissions" in res.metrics
+        ]
         total_client_emissions = sum(client_emissions) if client_emissions else 0.0
-        
+
         # Server-side emissions
         server_emissions = server_tracker.stop()
-        
+
         total_emissions = total_client_emissions + server_emissions
-        print(f"ROUND {server_round}. Total Carbon Emissions (server and clients): {total_emissions} kg CO2")
+        print(
+            f"ROUND {server_round}. Total Carbon Emissions (server and clients): {total_emissions} kg CO2"
+        )
 
         return aggregated_parameters, {"total_emissions": total_emissions}
 
-        
+
 if DP_BOOL is True:
     SAMPLED_CLIENTS = int(SAMPLED_CLIENTS)
     if METRIC_PRIVACY is True:
@@ -222,9 +246,12 @@ if DP_BOOL is True:
                 config=fl.server.ServerConfig(num_rounds=FEDERATED_ROUNDS),
                 strategy=AggregateEmissionsMDP(strategy),
             )
-        else:    
+        else:
             mdp_strategy = MetricDifferentialPrivacyServerSideFixedClipping(
-                strategy, noise_multiplier=float(NOISE_MULTIPLIER), clipping_norm=float(CLIPPING_NORM), num_sampled_clients=SAMPLED_CLIENTS
+                strategy,
+                noise_multiplier=float(NOISE_MULTIPLIER),
+                clipping_norm=float(CLIPPING_NORM),
+                num_sampled_clients=SAMPLED_CLIENTS,
             )
             fl.server.start_server(
                 server_address="0.0.0.0:5000",
@@ -240,8 +267,10 @@ if DP_BOOL is True:
             )
         else:
             dp_strategy = DifferentialPrivacyServerSideFixedClipping(
-                strategy, noise_multiplier=float(NOISE_MULTIPLIER), clipping_norm=float(CLIPPING_NORM), num_sampled_clients=SAMPLED_CLIENTS
-            
+                strategy,
+                noise_multiplier=float(NOISE_MULTIPLIER),
+                clipping_norm=float(CLIPPING_NORM),
+                num_sampled_clients=SAMPLED_CLIENTS,
             )
             fl.server.start_server(
                 server_address="0.0.0.0:5000",
